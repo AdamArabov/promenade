52b4a1b2db62d38de6c8f064684619f5e6fb6fdb
AdamArabov <arabovadam@yahoo.com>
2023-08-13 14:04:50 -0400

    contact done

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,30 @@
+import Card from "../components/card"
+
+
 export default function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
+        
+        <Card>
+        <div className="text-center">
+          <h1 className="font-bold text-xl">BUSINESS HOURS</h1>
+          <p className="text-xl">Monday-Thursday</p>
+          <p>12:00pm - 9:00pm</p>
+          <p className="font-semibold">
+            <span className="text-xl font-bold">Friday</span>
+            <br  />
+            12:00pm - 7:00pm
+          </p>
+          <p className="text-xl">Saturday- Sunday</p>
+          <p>12:00pm - 9:00pm</p>
         </div>
+        <div className="text-center">
+          <br />
+          <h1 className="font-bold text-xl">CONTACT US</h1>
+          <p>Phone:(718) 898-3215</p>
+          <p>Email: promenadewine@gmail.com</p>
+        </div>
+      </Card>
+    
+     
     )
 }
