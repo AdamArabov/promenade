223a8a7ccf2b02a968226656320e7ac69185b65d
AdamArabov <arabovadam@yahoo.com>
2023-08-13 13:16:14 -0400

    layout

---
+++ app/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+<footer className="footer self-end dark:bg-white bg-zinc-900 px-4 py-2  font-semibold text-center w-75%">
+62-06B Woodhaven Blvd, Queens, NY 11374 | Tel. (718) 898-3215 |  promenadewine@gmail.com
+    </footer>
+    )
+}
