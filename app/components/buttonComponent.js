62b65b087a6b9aa02b7e4e99ba08d1ccf6468c54
Adam Arabov <113316276+AdamArabov@users.noreply.github.com>
2023-08-14 14:32:09 -0400

    Update buttonComponent.js

--- app/components/buttonComponent.js
+++ app/components/buttonComponent.js
@@ -1,9 +1,6 @@
 export default function ButtonComponent() {
     return (
-      <button
-        id="scrollToBottomButton" 
-        className="mt-4 text-white border border-white transition duration-300 ease-in-out px-4 py-2 rounded"
-      >
+      <button className="mt-4 text-white border border-white transition duration-300 ease-in-out px-4 py-2 rounded">
         Scroll to Learn More
       </button>
     );
