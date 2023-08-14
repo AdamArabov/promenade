e40211fb8cd45c503ba307493f8282e96abcdd76
Adam Arabov <113316276+AdamArabov@users.noreply.github.com>
2023-08-14 15:38:46 -0400

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,20 +1,4 @@
-import Image from "next/image";
-import logo from "../assets/logo.png"
 
-export default function Navbar() {
-    return (
-      <div className="absolute top-0 left-0 p-4 z-10">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={150}
-          height={100}
-        />
-      </div>
-    );
-  } 
-  
-  
   
   
   
