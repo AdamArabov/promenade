e338f6205314b2dd4b2a6a89710ffab7137b598c
AdamArabov <arabovadam@yahoo.com>
2023-08-15 20:33:14 -0400

    t

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains : ['images.unsplash.com']
+  }
+}
 
-module.exports = {
-    images: {
-      domains: ['images.unsplash.com'], // Add other domains if needed
-    },
-  };
-  
+module.exports = nextConfig
